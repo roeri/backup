474d2cca681c702a2d7384f8a08403ded76abca7
robert <robert.eriksson@msn.com>
2018-02-03 04:52:12 -0800

    Adding sout just to try pushing with ssh.

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -5,6 +5,7 @@ import rsync.RSyncRunner;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Doing stuff now!");
         new RSyncRunner().doRsync();
     }
 }
